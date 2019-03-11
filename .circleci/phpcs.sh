8b5be65c0f57fc37f58b026dbfe3fe1a4fe60836
Gabor Javorszky <gabor@javorszky.co.uk>
2019-03-11 12:30:04 +0000

    Clean up phpcs.sh

--- .circleci/phpcs.sh
+++ .circleci/phpcs.sh
@@ -12,16 +12,12 @@ fi
 echo "Grabbing WordPress Coding Standards"
 git clone -b master https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git wpcs
 
-echo "Current working directory"
-pwd
-
 echo "Adding WPCS to phpcs path"
 ./vendor/bin/phpcs --config-set installed_paths $(pwd)/wpcs
 
 echo "Checking installed paths"
 ./vendor/bin/phpcs -i
 
-echo "Parsing PR URL for the PR number"
 regexp="[[:digit:]]\+$"
 PR_NUMBER=`echo $CIRCLE_PULL_REQUEST | grep -o $regexp`
 echo "PR number is $PR_NUMBER"
@@ -31,45 +27,25 @@ url="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPON
 if [[ -z $GITHUB_TOKEN ]];
 then
 	echo "GITHUB_TOKEN not set"
-else
-	echo "GITHUB TOKEN set"
+	exit 1
 fi
 
 target_branch=$(curl -X GET -G \
 $url \
 -d access_token=$GITHUB_TOKEN | jq '.base.ref' | tr -d '"')
 
-echo "PR number from circle is $PR_NUMBER <-"
-
 git checkout $target_branch
 
 git reset --hard origin/$target_branch
 
-git status
-
 git checkout $CIRCLE_BRANCH
-
-git status
-# echo "Fetching target branch: $target_branch"
-# git fetch origin
-
-# echo "Checking out target branch"
-# git checkout $target_branch
-# git checkout $CIRCLE_BRANCH
-
-echo "Getting changed files:"
-echo "git diff --name-only $target_branch..$CIRCLE_BRANCH -- '*.php'"
 
 changed_files=$(git diff --name-only $target_branch..$CIRCLE_BRANCH -- '*.php')
 
-echo "Changed files"
-echo $changed_files
-echo "End of changed files"
-
 if [[ -z $changed_files ]]
 then
-	echo "There are no files to check, boo."
+	echo "There are no files to check."
 	exit 0
 fi
 
-./vendor/bin/phpcs --standard="WordPress-Extra" $changed_files
+./vendor/bin/phpcs $changed_files
